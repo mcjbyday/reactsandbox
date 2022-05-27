24700c651e269e3cca1b8671e9ba32dcadd670c4
MCJBYDAY <jacobsmichaelc@gmail.com>
2022-05-27 16:20:42 -0500

    updated some components for artists

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+let artists = [
+  {name:"Duo Brothers", favoriteSong:"Dopefully", huenique:"brown"},
+  {name: "War on Drugs", favoriteSong:"Red Eyes", huenique:"purple"},  
+  {name:"DJ Seinfeld",  favoriteSong:"She Loves Me", huenique:"pink"}, 
+  {name:"Steely Dan", favoriteSong:"Aja",  huenique:"blue"},  
+  {name:"Chance the Rapper",  favoriteSong:"Acid Rain", huenique:"red"}
+];
 
 function App() {
+  
+  let [artistOfDay, setArtistOfDay] = useState(artists[1]);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <p style={{color: artistOfDay.huenique}}>My favorite song today is: {setArtistOfDay.name}</p>
+      <ListComponent setArtistOfDay={setArtistOfDay} artists={artists}> </ListComponent>
+      hello world! 
     </div>
   );
 }
+
+function ListComponent({artists, setArtistOfDay}) { 
+    return (
+      <ul>
+          {artists.map((artist) => <ArtistItemComponent setArtistOfDay={setArtistOfDay} artist={artist}></ArtistItemComponent>)}
+      </ul>
+    )
+  }
+
+
+function ArtistItemComponent({artist, setArtistOfDay}) { 
+    return (
+      <li onClick={() => setArtistOfDay(artist)} style={{color: artist.huenique}}>{artist.name}</li>
+    )
+  }
+
 
 export default App;
